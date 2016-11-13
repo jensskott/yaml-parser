ffbe305202b1f8e012e9ea390742c30967539397
Jens Skott <jens.skott@gmail.com>
2016-11-13 20:13:59 +0100

    now it might work?

--- parse.rb
+++ parse.rb
@@ -1,15 +1,16 @@
 require 'yaml'
 require 'json'
+require 'deep_merge'
 
 yaml = Psych.load_stream(open('def.yml'))
 profile = 'production'
 
-class ::Hash
-    def deep_merge(second)
-        merger = proc { |key, v1, v2| Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : Array === v1 && Array === v2 ? v1 | v2 : [:undefined, nil, :nil].include?(v2) ? v1 : v2 }
-        self.merge(second.to_h, &merger)
-    end
-end
+#class ::Hash
+#    def deep_merge(second)
+#        merger = proc { |key, v1, v2| Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : Array === v1 && Array === v2 ? v1 | v2 : [:undefined, nil, :nil].include?(v2) ? v1 : v2 }
+#        self.merge(second.to_h, &merger)
+#    end
+#end
 
 l = yaml.length
 i = 0
@@ -23,7 +24,8 @@ while i < l  do
    i +=1
 end
 
-h3 = h2.deep_merge(h1)
+h3 = h1.merge!(h2)
+#h3 = h2.deep_merge(h1)
 #h1.merge!(h2)
 #h3.delete('profile')
 puts JSON.pretty_generate(h3)
