598b7772113109fdf9c699fa4fa62ec6ac54bec2
Jens Skott <jens.skott@gmail.com>
2016-11-13 20:09:59 +0100

    closer and closer

--- parse.rb
+++ parse.rb
@@ -1,6 +1,15 @@
 require 'yaml'
+require 'json'
+
 yaml = Psych.load_stream(open('def.yml'))
 profile = 'production'
+
+class ::Hash
+    def deep_merge(second)
+        merger = proc { |key, v1, v2| Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : Array === v1 && Array === v2 ? v1 | v2 : [:undefined, nil, :nil].include?(v2) ? v1 : v2 }
+        self.merge(second.to_h, &merger)
+    end
+end
 
 l = yaml.length
 i = 0
@@ -13,6 +22,8 @@ while i < l  do
    end
    i +=1
 end
-h1.merge!(h2)
-h1.delete('profile')
-puts h1
+
+h3 = h2.deep_merge(h1)
+#h1.merge!(h2)
+#h3.delete('profile')
+puts JSON.pretty_generate(h3)
